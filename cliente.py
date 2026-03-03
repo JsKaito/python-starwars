f881d000a106b5c20b5ceaa6b6b187adb8d9ed68
Kaito <fernandoruizmurciano@gmail.com>
2026-03-03 12:25:45 +0100

    ♻️ Actualiza servidor.py desde fernando y cliente.py desde Lucia

---
+++ cliente.py
@@ -0,0 +1,35 @@
+import socket
+
+
+def run_client():
+    """
+    Cliente para un servidor TCP.
+    Envía conexiones a un servidor, envía mensajes y espera respuestas.
+    Si envía 'close', cierra la conexión. 
+    """
+    # Crea un socket
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_ip = "127.0.0.1"  
+    server_port = 8000  
+    
+    # Establece la conexión con el servidor
+    client.connect((server_ip, server_port))
+
+    while True:
+        msg = input("Introduce el mensaje: ")
+        client.send(msg.encode("utf-8")[:1024])
+
+        response = client.recv(1024)
+        response = response.decode("utf-8")
+
+        if response.lower() == "closed":
+            break
+
+        print(f"Recibido: {response}")
+
+    # Cierra la conexión con el servidor
+    client.close()
+    print("Conexión con el servidor cerrada.")
+
+run_client()
