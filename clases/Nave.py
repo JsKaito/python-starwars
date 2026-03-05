05da72f593a195041feded887f9dff8712ac6a42
Fernando <fernandoruizmurciano@gmail.com>
2026-03-05 16:57:08 +0100

    Refactor crearNaves method to initialize ships using a list for better readability

--- clases/Nave.py
+++ clases/Nave.py
@@ -13,8 +13,10 @@ class Nave:
         self.coste = coste
 
     def crearNaves(self):
-        nave1 = Nave(1, "Estrella de la Muerte", 80, 90, 1500, rd.randint(20, 30), 4500)
-        nave2 = Nave(2, "Ejecutor", 70, 80, 1200, rd.randint(35, 50), 4000)
-        nave3 = Nave(3, "Halcón Milenario", 60, 50, 800, 70, 2500)
-        nave4 = Nave(4, "Nave Real de Naboo", 40, 60, 600, 50, 2000)
-        nave5 = Nave(5, "Caza Estelar Jedi", 50, 40, 400, 80, 1500)
+        naves = [
+            Nave(1, "Estrella de la Muerte", 80, 90, 1500, rd.randint(20, 30), 4500),
+            Nave(2, "Ejecutor", 70, 80, 1200, rd.randint(35, 50), 4000),
+            Nave(3, "Halcón Milenario", 60, 50, 800, 70, 2500),
+            Nave(4, "Nave Real de Naboo", 40, 60, 600, 50, 2000),
+            Nave(5, "Caza Estelar Jedi", 50, 40, 400, 80, 1500)
+        ]
