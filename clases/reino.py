6e913f43e65b96626924578c9bdf2aa771bda350
Lucia <jimenezmoralesluci@alumnos.alborfp.com>
2026-03-16 12:38:28 +0100

    Añadidos  reino (nave y preparación de mandaloriano)

--- clases/reino.py
+++ clases/reino.py
@@ -1,9 +1,22 @@
 from Nave import Nave
+from Mandaloriano import Mandaloriano
 naves = Nave.crearNaves(self=None)
+mandaloriano = Mandaloriano.crearMandalorianos(self=None)
 
 cantidadNaves = [0, 2, 2, 4, 10]
+cantidadMandalorianos = [0, 2, 2, 4, 10]
 precioTotal = 0 
-for i in range(len(cantidadNaves)):
-    precioTotal += cantidadNaves[i] * naves[i].coste
+
+while True:
+    for i in range(len(cantidadNaves)):
+        precioTotal += cantidadNaves[i] * naves[i].coste
+        precioTotal += cantidadMandalorianos[i] * mandaloriano[i].coste
+        
+    print(f"El precio total es: {precioTotal}")
+        
+    if precioTotal < 100000:
+        break
+    print("El precio total supera los 100000 créditos.Vuelve a introducir las cantidades.")
     
-print(f"El precio total de las naves es: {precioTotal}")
+    
+    
