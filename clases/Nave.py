003cd0eace374bf8ce162eb513d43f7a8c2a7f7a
Kaito <fernandoruizmurciano@gmail.com>
2026-03-16 11:46:00 +0100

    Corrige la inicialización de la velocidad en el método crearNaves para usar tuplas en lugar de valores individuales

--- clases/Nave.py
+++ clases/Nave.py
@@ -14,9 +14,9 @@ class Nave:
 
     def crearNaves(self):
         naves = [
-            Nave(1, "Estrella de la Muerte", 80, 90, 1500, rd.randint(20, 30), 4500),
-            Nave(2, "Ejecutor", 70, 80, 1200, rd.randint(35, 50), 4000),
-            Nave(3, "Halcón Milenario", 60, 50, 800, 70, 2500),
-            Nave(4, "Nave Real de Naboo", 40, 60, 600, 50, 2000),
-            Nave(5, "Caza Estelar Jedi", 50, 40, 400, 80, 1500)
+            Nave(1, "Estrella de la Muerte", 80, 90, 1500, (20, 30), 4500),
+            Nave(2, "Ejecutor", 70, 80, 1200, (35, 50), 4000),
+            Nave(3, "Halcón Milenario", 60, 50, 800, (70, 70), 2500),
+            Nave(4, "Nave Real de Naboo", 40, 60, 600, (50, 50), 2000),
+            Nave(5, "Caza Estelar Jedi", 50, 40, 400, (80, 80), 1500)
         ]
