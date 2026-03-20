97dbef01f632504416fdd9ac53691da6771d1c17
Kaito <fernandoruizmurciano@gmail.com>
2026-03-20 12:27:54 +0100

    Add Mandaloriano and update Nave classes with improved initialization and creation methods

--- clases/nave.py
+++ clases/nave.py
@@ -2,18 +2,19 @@ import random as rd
 class Nave:
     """Representa una nave en el universo Star Wars."""
 
-    def __init__(self, id, nombre, ataque, defensa, vida, velocidad, coste):
-        """Inicializa una nave."""
+    def __init__(self, id, name, attack, defense, hp, speed, price):
+        """Inicializa naves."""
         self.id = id
-        self.nombre = nombre
-        self.ataque = ataque
-        self.defensa = defensa
-        self.vida = vida
-        self.velocidad = velocidad
-        self.coste = coste
+        self.name = name
+        self.attack = attack
+        self.defense = defense
+        self.hp = hp
+        self.speed = speed
+        self.price = price
 
-    def crearNaves(self):
-        naves = [
+    @staticmethod
+    def crearNaves():
+        return [
             Nave(1, "Estrella de la Muerte", 80, 90, 1500, (20, 30), 4500),
             Nave(2, "Ejecutor", 70, 80, 1200, (35, 50), 4000),
             Nave(3, "Halcón Milenario", 60, 50, 800, (70, 70), 2500),
